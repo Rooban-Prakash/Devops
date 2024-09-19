53aa42b034299d113e5ce77f3274ed76d3b2c854
Rooban-Prakash <roobanprakash2001@gmail.com>
2024-09-19 10:24:29 +0530

    create file is modified

---
+++ create.sh
@@ -0,0 +1,7 @@
+  if condition; then
+     # true code
+  elif another_condition; then
+     # condition is false, and another_condition is true
+  else
+     # none of the above conditions are true
+  fi
